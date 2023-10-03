9d093facbfeebf2429778621242a1318e8971815
thcrt <110127860+thcrt@users.noreply.github.com>
2023-10-03 13:24:49 +0200

    Add page navigation

--- hzine/pages.py
+++ hzine/pages.py
@@ -1,9 +1,16 @@
 import flask as f
 from flask import current_app as app
+from jinja2.exceptions import TemplateNotFound
 
 pages = f.Blueprint("pages", __name__, template_folder="templates")
 
 
-@pages.route("/")
-def home():
-    return "Hello, world!"
+@pages.route("/", defaults={"path": "index"})
+@pages.route("/<path:path>")
+def catch_all(path):
+    try:
+        if path.startswith("_"):
+            raise TemplateNotFound(path)
+        return f.render_template(f"{path}.html")
+    except TemplateNotFound:
+        return f"Not found: {path}", 404
