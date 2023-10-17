1dd47ccf114f267ce0b03fcaca6bc7a98699d06a
thcrt <110127860+thcrt@users.noreply.github.com>
2023-10-17 15:37:42 +0200

    Create SystemD service file

--- hzine/__main__.py
+++ hzine/__main__.py
@@ -1,6 +1,9 @@
+import os
+
 from waitress import serve
-from website import create_app
+
+from hzine import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    serve(app, host="0.0.0.0", port="9000")
+    serve(app, host="127.0.0.1", port=os.environ.get("PORT", 8080))
